e88146a7ce5983a82bb68517f68faf60b078773b
railsfactory-udhaya <udhaya@railsfactory.org>
2012-01-26 15:22:05 +0530

    Initial copy

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsMine::Application.config.secret_token = '31716ae0d5639e77f7b71bca327b1bc04fe7e57b1e9b501a89c7d5f42a3d99b303d66c51d08dfebff11b7cc1dc531fe1e1e5c76c4633ff0b27633136d1b2d181'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RailsMine::Application.config.session_store :cookie_store, key: '_RailsMine_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RailsMine::Application.config.session_store :active_record_store
